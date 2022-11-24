16d6d0d886cfb6a29464a825d96b0869570d60ca
Kazzspec <kazzn40@gmail.com>
2022-11-23 20:56:44 -0500

    Se agrega carrito de compras

--- main.js
+++ main.js
@@ -3,16 +3,35 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon= document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cardsContainer = document.querySelector('.cards-container')
+const cartIcon = document.querySelector('.navbar-shopping-cart')
+const productDetail =  document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleCartIcon);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+    if (!isProductDetailClosed){
+    productDetail.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+
+function toggleCartIcon(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    productDetail.classList.toggle('inactive')
+
 }
 
 const productList = [];
@@ -52,25 +71,6 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
-
-
-/*<div class="cards-container">
-<div class="product-card">
-    <img
-        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        alt=""
-    />
-    <div class="product-info">
-        <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-        </div>
-        <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="" />
-        </figure>
-    </div>
-</div>
-</div>*/
 
 for (product of productList){
    const productCard = document.createElement('div');
